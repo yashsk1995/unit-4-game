d82f87f8fe1407f84cf550144cbab1a0e5ac0624
yashK <yashkinariwalask95@gmail.com>
2018-06-12 11:01:38 -0400

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,49 @@ var wins=0;
 var losses=0;
  
 
-// alert(random);
 $("#computer-Guess").html(computerGuess);
 $("#win").html(wins);
 $("#losses").html(losses);
 $("#counter").html(counter);
+newvaule();
 
+function newvaule(){
 for(var i=1;i < 5;i++){
     var random=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     $("#img"+[i]).attr("value1",random);
 }
+}
 
+
+$(".img").on("click" , function () {
+    var crystalValue = ($(this).attr("value1"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    $("#counter").html(counter);
+    
+
+    if(counter === computerGuess){
+        wins++;
+        $("#win").html(wins);
+        // alert("win");
+        computerGuess=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#computer-Guess").html(computerGuess);
+
+        newvaule();
+        counter=0;
+        $("#counter").html(counter);
+        
+    }
+    else if (counter >= computerGuess) {
+        // alert("You lose!!");
+        losses++;
+        $("#losses").html(losses);
+        computerGuess=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#computer-Guess").html(computerGuess);
+
+        newvaule();
+        counter=0;
+        $("#counter").html(counter);
+                
+      }
+});
